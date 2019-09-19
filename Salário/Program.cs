0fac5683fbf3fed902379ba99a4db98b85259f55
Mateus Braz <mateusbrazalmeida@hotmail.com>
2019-09-19 17:25:27 -0300

    Exercicios

---
+++ Salário/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Salário
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario, salarionovo;
+            Console.Write("Digite o sálario: ");
+            salario = double.Parse(Console.ReadLine());
+
+            if(salario<500){
+            salarionovo=salario+(salario* 0.30);
+            Console.WriteLine("O sálario NOVO é: " + salarionovo);
+            }
+            else Console.WriteLine("Não houve aumento");
+        }
+    }
+}
