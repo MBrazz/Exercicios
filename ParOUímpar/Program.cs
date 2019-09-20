0fd9b26ced3657a57103f0cb6450407aeddb3abd
Mateus Braz <mateusbrazalmeida@hotmail.com>
2019-09-20 17:29:21 -0300

    Exercícios de Par e Ímpar

---
+++ ParOUímpar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ParOUímpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int num = 0;
+             do{
+
+             Console.Write("Digite um número: ");
+
+             num = int.Parse(Console.ReadLine());
+
+             if(num % 2 == 0) {
+                  Console.WriteLine("O número é par");
+              } 
+              else {
+                  Console.WriteLine("O número é ímpar");
+              }
+             
+              } while (num != 0); {
+              Console.WriteLine("FIM");
+            }
+  
+
+            
+
+        }
+    }
+}
