543b9a9165a108bdf1a65a1b0e5e2394dc963146
Mateus Braz <mateusbrazalmeida@hotmail.com>
2019-10-18 17:19:17 -0300

    ByteBank 2

---
+++ ByteBank2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ByteBank2.Models;
+
+namespace ByteBank2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaCorrente1 = new ContaCorrente(1, 1, "Alexandre");
+            ContaEspecial contaEspecial1 = new ContaEspecial(1, 2, "Cesar");
+        }
+    }
+}
